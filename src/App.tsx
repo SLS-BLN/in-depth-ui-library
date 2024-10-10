5bf841b9fa829b15ab31243df3759ecd3fb23f64
Stefan Schlicker <stefanschlicker@posteo.de>
2024-10-10 19:50:10 +0200

    eslint 8 instead of 9 to work with storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Button } from "./stories/Button";
+import { Header } from "./stories/Header";
+
 function App() {
   return (
     <>
+      <Header
+        onCreateAccount={() => {}}
+        onLogin={() => {}}
+        onLogout={() => {}}
+        user={{
+          name: "Stefan Schlicker",
+        }}
+      />
       <h1>Hello World</h1>
+      <Button label="Get in Contact" onClick={() => {}} primary size="large" />
     </>
   );
 }
